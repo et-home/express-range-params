d790633999cf4bee8f909fe1616ab974db8094c7
Ethan Tao <eth@ntao.ca>
2022-03-10 21:04:27 -0500

    add valueOf()

--- middleware/uname.js
+++ middleware/uname.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 let uname = (req, res, next) => {
   if (req.query.uname) {
-    req.username = req.query.uname.trim().toLowerCase();
+    req.username = req.query.uname.trim().toLowerCase().valueOf();
   } else {
     req.username = null;
   }
